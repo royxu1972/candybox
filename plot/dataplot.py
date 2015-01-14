6fecfdf307d752942f49ac532c66b4d21475928e
Huayao <hywu@outlook.com>
2015-01-14 19:44:40 +0800

    0114

--- plot/dataplot.py
+++ plot/dataplot.py
@@ -1,64 +1,137 @@
 import matplotlib.pyplot as plt
 
-font_large = {'family':'Arial', 'size':20}
-font_normal = {'family':'Arial', 'size':16}
-linestyle = ["kv-", "ko-", "ks-", "kd-", "k+-"]
+font_large = {'family': 'Arial', 'size': 20}
+font_normal = {'family': 'Arial', 'size': 16}
+line_style = ["kv-", "ko-", "ks-", "kd-", "k+-"]
 
-filename1 = "D:\\Workspace\\CombinatorialDesign\\tmp\\data"
+filename1 = "D:\\Workspace\\CombinatorialDesign\\result\\data"
+filename2 = "D:\\Workspace\\CombinatorialDesign\\result\\data-box"
 
-class myplot:
-	#def __init__(self):
 
-	''' data format
-	mean results, xlabel, ylabel
-	0.1 0.2 0.3 0.4 0.5
-	d1 1 2 3 4 5
-	d2 2 3 2 1 4
-	'''
-	def lineplot(self, filename):
-		data = []
-		label = []
-		xsticks = []
-		# read file
-		with open(filename, "r") as f:
-			lines = f.readlines()
-			# set title, xlabel, ylabel
-			l1 = lines[0].strip().split(',')
-			title = l1[0]
-			xlabel = l1[1]
-			ylabel = l1[2]
+class Data:
+        title = ""
+        xlabel = ""
+        ylabel = ""
+        xsticks = []
+        data = []
+        legend = []
+        def __init__(self, title, x, y, xs, data, legend):
+            self.title = title
+            self.xlabel = x
+            self.ylabel = y
+            self.xsticks = xs
+            self.data = data
+            self.legend = legend
 
-			# set xsticks
-			xtp = lines[1].strip().split()
-			for k in range(0, len(xtp)):
-				xsticks.append(xtp[k])
-			x = [k for k in range(0, len(xtp))]
 
-			# set data
-			for k in range(2, len(lines)):
-				tp = lines[k].strip().split()
-				label.append(tp[0])
+class MyPlot:
 
-				d = []
-				for y in range(1, len(tp)):
-					d.append( float(tp[y]) )
-				data.append(d)
+    def __init__(self):
+        print("do plot")
 
-		# do plot
-		plt.figure(figsize=(14,8))
-		for index in range(0, len(data)):
-			plt.plot(data[index], linestyle[index], markerfacecolor='none', ms=6.5, mew=2.5, label=label[index], linewidth=1.5)
+    # do a single line plot
+    def line(self, Data):
+        plt.figure(figsize=(14, 8))
+        for index in range(0, len(Data.data)):
+            plt.plot(Data.data[index], line_style[index], label=Data.legend[index],
+                     markerfacecolor='none', ms=6.5, mew=2.5,
+                     linewidth=1.5)
 
-		plt.xlabel(xlabel, fontdict=font_normal)
-		plt.ylabel(ylabel, fontdict=font_normal)
-		#plt.title(title, fontdict=font_large)
-		plt.xticks(x, xsticks)
-		plt.legend(loc='best', numpoints=1, fancybox=True)
-		plt.tight_layout()
-		plt.show()
+        plt.xlabel(Data.xlabel, fontdict=font_normal)
+        plt.ylabel(Data.ylabel, fontdict=font_normal)
+        # plt.title(title, fontdict=font_large)
+        x = [k for k in range(0, len(Data.xsticks))]
+        plt.xticks(x, Data.xsticks)
+        plt.legend(loc='best', numpoints=1, fancybox=True)
+        plt.tight_layout()
+        plt.show()
 
-	#def boxplot(self, filename):
+    # do multiple box plot
+    def boxes(self, Datas, column):
+        row = len(Datas) // column
+        fig, axes = plt.subplots(nrows=row, ncols=column)
 
-# run
-mp = myplot()
+        index = 0
+        for i in range(0, row):
+            for j in range(0, column):
+                axes[i, j].boxplot(Datas[index].data, labels=Datas[0].xsticks,
+                                   showmeans=True, meanline=True)
+                axes[i, j].set_title(Datas[index].title)
+                index = index + 1
+
+        #for ax in axes.flatten():
+        #    ax.set_yscale('log')
+        #    ax.set_yticklabels([])
+
+        fig.subplots_adjust(hspace=0.3)
+        plt.show()
+
+
+    def lineplot(self, filename):
+        '''
+        title, xlabel, ylabel
+	    0.1 0.2 0.3 0.4 0.5
+	    d1 1 2 3 4 5
+	    d2 2 3 2 1 4
+        '''
+        with open(filename, "r") as f:
+            lines = f.readlines()
+            # l = [title, xlabel, ylabel]
+            l = lines[0].strip().split(',')
+            xsticks = lines[1].strip().split()
+
+            # data
+            legend = []
+            data = []
+            for k in range(2, len(lines)):
+                tp = lines[k].strip().split()
+                legend.append(tp[0])
+
+                d = []
+                for y in range(1, len(tp)):
+                    d.append(float(tp[y]))
+                data.append(d)
+
+            # save results
+            pdata = Data(l[0].strip(), l[1].strip(), l[2].strip(), xsticks, data, legend)
+            self.line(pdata)
+
+    def boxplot(self, filename):
+        '''
+        title, xlabel, ylabel
+	    1 2 3 4 5
+	    2 3 2 1 4
+        '''
+        allData = []
+        f = open(filename, "r")
+        while ( True ):
+            line = f.readline()
+            if ( not line ):
+                break
+
+            # read data
+            l = line.strip().split(',')
+            xs = []
+            data = []
+            while ( True ):
+                line = f.readline()
+                # blank line, end
+                if( len(line) == 1 ):
+                    break
+                # data lines
+                line = line.strip().split()
+                xs.append( line[0] )    # xsticks[i]
+                d = []
+                for n in range(1, len(line)):
+                    d.append(float(line[n]))
+                data.append(d)
+            bdata = Data(l[0].strip(), l[1].strip(), l[2].strip(), xs,data,[])
+            allData.append(bdata)
+
+        print("all %d data" % len(allData))
+        self.boxes(allData,6)
+
+
+mp = MyPlot()
 mp.lineplot(filename1)
+#mp.boxplot(filename2)
--- readme.md
+++ readme.md
@@ -1,19 +1,1 @@
 # Candy Box
-
-candy box is a collection of little scripts for different applications
-
-### bibParse
-
-parsing .bib file for paper repository
-
-### paperlist
-
-export plain paper list from database
-
-### crawl
-
-simple crawl programs
-
-### Feng
-
-experimental data processing for Feng's personal use
