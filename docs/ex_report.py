23aa60e7bc35b5e525ec2156d7ef6f4304c8ccb0
Huayao <hywu@outlook.com>
2015-03-08 15:54:49 +0800

    20150308

--- docs/ex_report.py
+++ docs/ex_report.py
@@ -1,6 +1,8 @@
 from docx import Document
 from docx.shared import Inches
 import os
+import re
+import sys
 
 PATH = "D:\\Work CT Application\\report\\"
 
@@ -22,6 +24,15 @@ def convert(filename):
 		output += "<tr><th>" + item + "</th><td>" + content + "</td></tr>\n"
 	output += "</table>"
 
+	# replace "^"
+	strinfo = re.compile('\^\d+\s*')
+	c = strinfo.findall(output)
+	for i in range(0, len(c)):
+		c[i] = c[i][1:]
+		c[i] = c[i].strip()
+	for i in range(0, len(c)):
+		output = strinfo.sub('<sup>' + c[i] + '</sup>', output, 1)
+
 	# write
 	tp = filename[0:filename.find('.')].split()
 	name = tp[0] + tp[1]
@@ -29,6 +40,43 @@ def convert(filename):
 	f1.write(output)
 	f1.close
 	return name
+
+
+def appendcontent():
+	# SPL
+	f = open('1.txt')
+	all_text = f.read()
+	f.close()
+	f = open( "D:\\Workspace\\DocApplication\\docs\\2006SPL.html",'a')
+	f.write(all_text)
+	f.close
+
+	# real cases
+	f = open('2.txt')
+	all_text = f.read()
+	f.close()
+	f = open( "D:\\Workspace\\DocApplication\\docs\\2011real.html",'a')
+	f.write(all_text)
+	f.close
+
+
+
+
+
+'''
+t = "posdad 5^1 2^128 3^4, ddd 9^10 2^1"
+strinfo = re.compile('\^\d+\s*')
+c = strinfo.findall(t)
+for i in range(0, len(c)):
+	c[i] = c[i][1:]
+	c[i] = c[i].strip()
+print(c)
+
+for i in range(0, len(c)):
+	t = strinfo.sub('<sup>' + c[i] + '</sup>', t, 1)
+print(t)
+sys.exit(0)
+'''
 
 '''
 main
@@ -44,3 +92,4 @@ for root, dirs, files in list_dirs:
 f = open( "list.html",'w')
 f.write(outlist)
 f.close
+appendcontent()
