2ab1ae9a4ec9854638ba842052db1b8242f0ab0f
huayao <hywu@outlook.com>
2015-03-25 13:31:01 +0800

    0325 version

--- PyRepository/export.py
+++ PyRepository/export.py
@@ -123,3 +123,24 @@ class paper:
 
             oracle = oracle.encode('UTF-8')
             wfile.writelines("oracle: " + oracle.decode('gbk') + "\n")
+
+
+
+if __name__=='__main__':
+    # do export
+    p = paper()
+    p.get_first_author()
+
+'''
+import os
+
+path = "C:\\Users\\Huayao\\Desktop\\All"
+for root, dir, list in os.walk(path):
+	# root遍历路径，dirs当前遍历路径下的目录，list当前遍历目录下的文件名
+    for i in list:
+    	pos = i.index('.')
+    	name = i[pos+1:]
+
+    	print(name)
+    	os.rename(path+"\\"+i, path+"\\"+name)
+'''
